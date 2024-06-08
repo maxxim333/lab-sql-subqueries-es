3f5d0c2547e2ac5d7e3f623275116ff6e52b35e1
maxxim333 <biomedmax@protonmail.com>
2024-06-08 13:54:08 +0200

    all answers

--- answers_maksym_vaskin.sql
+++ answers_maksym_vaskin.sql
@@ -43,5 +43,47 @@ WHERE def = "Canada";
 
 
 -- ¿Cuáles son las películas protagonizadas por el actor más prolífico? El actor más prolífico se define como el actor que ha actuado en el mayor número de películas. Primero tendrás que encontrar al actor más prolífico y luego usar ese actor_id para encontrar las diferentes películas en las que ha protagonizado.
+SELECT actor_id, COUNT(film_id) AS count FROM film_actor
+GROUP BY actor_id
+ORDER BY count DESC
+LIMIT 1; -- Es el actor con ID 107
+
+SELECT film.title FROM film_actor
+JOIN actor ON actor.actor_id = film_actor.actor_id
+JOIN film ON film.film_id = film_actor.film_id
+WHERE film_actor.actor_id = 107;
+
+
+
 -- Películas alquiladas por el cliente más rentable. Puedes usar la tabla de clientes y la tabla de pagos para encontrar al cliente más rentable, es decir, el cliente que ha realizado la mayor suma de pagos.
+SELECT customer_id, SUM(AMOUNT) AS sum FROM payment
+GROUP BY customer_id
+ORDER BY sum DESC
+LIMIT 1; -- es el 526
+
+SELECT DISTINCT film.title FROM rental
+JOIN inventory ON inventory.inventory_id = rental.inventory_id
+JOIN film ON film.film_id = inventory.film_id
+WHERE customer_id = 526;
+
+
+
 -- Obtén el client_id y el total_amount_spent de esos clientes que gastaron más que el promedio del total_amount gastado por cada cliente.
+
+
+SET @average_length := (
+    SELECT AVG(avg_by_user) 
+    FROM (
+        SELECT AVG(amount) AS avg_by_user 
+        FROM payment 
+        GROUP BY customer_id
+    ) AS promedio
+);
+
+WITH avg_by_user_2 AS (
+	SELECT customer_id, AVG(amount) AS avg_by_user 
+	FROM payment 
+	GROUP BY customer_id
+    )
+SELECT * from avg_by_user_2
+WHERE avg_by_user > @average_length;
