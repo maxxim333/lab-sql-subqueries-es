f5d9978a7b016bca97ceedafd3fa6866c6d2a32f
maxxim333 <biomedmax@protonmail.com>
2024-06-08 11:55:50 +0200

    some more answers

--- answers_maksym_vaskin.sql
+++ answers_maksym_vaskin.sql
@@ -19,12 +19,29 @@ SELECT actor.first_name, actor.last_name FROM true_film_id
 JOIN film_actor ON film_actor.film_id = true_film_id.film_id
 JOIN actor ON actor.actor_id = film_actor.actor_id;
 
-
-
-
-
 -- Las ventas han estado disminuyendo entre las familias jóvenes, y deseas dirigir todas las películas familiares a una promoción. Identifica todas las películas categorizadas como películas familiares.
+SELECT film.title, category.name FROM category
+JOIN film_category ON film_category.category_id = category.category_id
+JOIN film ON film.film_id = film_category.film_id
+WHERE category.name = "Family";
+
 -- Obtén el nombre y correo electrónico de los clientes de Canadá usando subconsultas. Haz lo mismo con uniones. Ten en cuenta que para crear una unión, tendrás que identificar las tablas correctas con sus claves primarias y claves foráneas, que te ayudarán a obtener la información relevante.
+SELECT customer.first_name, customer.last_name, customer.email, country.country FROM address
+JOIN city ON city.city_id = address.city_id
+JOIN country ON country.country_id = city.country_id
+JOIN customer ON customer.address_id = address.address_id
+WHERE country.country = "Canada";
+
+WITH joined AS (
+SELECT customer.first_name, customer.last_name, customer.email, country.country AS def FROM address
+JOIN city ON city.city_id = address.city_id
+JOIN country ON country.country_id = city.country_id
+JOIN customer ON customer.address_id = address.address_id
+)
+SELECT * FROM joined
+WHERE def = "Canada";
+
+
 -- ¿Cuáles son las películas protagonizadas por el actor más prolífico? El actor más prolífico se define como el actor que ha actuado en el mayor número de películas. Primero tendrás que encontrar al actor más prolífico y luego usar ese actor_id para encontrar las diferentes películas en las que ha protagonizado.
 -- Películas alquiladas por el cliente más rentable. Puedes usar la tabla de clientes y la tabla de pagos para encontrar al cliente más rentable, es decir, el cliente que ha realizado la mayor suma de pagos.
 -- Obtén el client_id y el total_amount_spent de esos clientes que gastaron más que el promedio del total_amount gastado por cada cliente.
